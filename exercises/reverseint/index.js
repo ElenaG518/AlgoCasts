697a1cc0d12d14134f30e6829b30b39ac8262827
Elena Granados <patty518@yahoo.com>
2019-05-02 14:29:49 -0700

    solutions for reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {  
+// convert n to a string, then string to an array, then reverse   array, then back to a string
+  const reversed = n.toString().split('').reverse().join('');
 
+// turn string into number, which will get rid of any characters that are not numbers, in this case the sign, and delete 
+// any leading zeros.  then multiply by the sign 
+// parseInt will return NaN if first character in string is not a number
+return parseInt(reversed) * Math.sign(n);
+// debugger;
+}
+
+// reverseInt(-518);
 module.exports = reverseInt;
