41df13657ec4208b6cbb74eee8c6254cda9211ac
Elena Granados <patty518@yahoo.com>
2019-05-02 14:30:27 -0700

    solutions for palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,59 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  let i = 0;
+  let j = str.length - 1;
+ 
+  while (i <= j) {
+    if (str[i] !== str[j]) {
+      return false;
+    }
+    i++;
+    j--;
+  }
+ 
+  return true;
+
+
+// reverse and reduce are array methods
+  // const reversed = str.split('').reverse().join('');
+  // return str === reversed;  
+}
+
+// palindrome("penned");
 
 module.exports = palindrome;
+
+
+// solution 1
+// let newString = str.split('').reduce((rev, char) => {rev = char + rev}, '');
+// return newString===string;
+
+// Solution 1.2 
+  // return str.split('').reduce((acc, cur) => cur + acc) ===str;
+  
+
+// Solution 2
+// let newString ='';
+//   for (let char of str) {
+//     newString = char + newString;
+// }
+// return str===newString;
+
+// Solution 3
+  // array.every(function(currentValue(req), index(opt), arr(opt)), thisValue(opt))
+  // return str.split('').every((char, index) => {
+  //   return char === str[str.length -index -1];
+  // })  
+
+
+// Solution worst time
+  // for (let i = Math.floor(str.length / 2); i > -1; i--) {
+  //   if (str[i] != str[str.length - i - 1]) {
+  //     return false;
+  //   }
+  // }
+  // return true;
+  
+  
