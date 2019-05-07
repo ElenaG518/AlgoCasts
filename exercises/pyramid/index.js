77b55eef4d4f1d8c11aa5f5436e6ff93d3e6900e
Elena Granados <patty518@yahoo.com>
2019-05-06 19:31:46 -0700

    completed pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,40 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  // if we have reached base case and end of the pyramid, exit function
+  if(n=== row) {
+    return;
+  }
+
+  // if string recheases maximum length, print new string and start a new row 
+  if (2*n-1 === str.length) {
+    console.log(str);
+    return pyramid(n, row +1);
+  }
+
+  // otherwise work on adding more characters to string
+  if (str.length )
+
+}
 
 module.exports = pyramid;
+
+
+// function pyramid(n) {
+//   const midpoint = Math.floor((2*n-1)/2);
+  
+//   for (let row = 0; row < n; row ++) {
+//     let level = '';
+    
+//     for (let col=0; col< 2*n-1 ; col++) {
+//       if((col >= midpoint - row) && (col <= midpoint + row)) {
+//         level+="#";
+//       }
+//       else {
+//         level+=' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
