fffa47eecddf4f33e005a5bb130204c3aa00d4cd
Elena Granados <patty518@yahoo.com>
2019-05-14 10:55:19 -0700

    completed chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,30 +9,34 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
+  // create new array to hold the chunks
   const newArr = [];
+  // starting from zero and moving in segments of the desired size
   for (let x=0; x<array.length; x+=size) {
+    // get chunks of desired size from current array, adn then add the chunk to the new array
+    console.log(x);
     newArr.push(array.slice(x,x+size));
 
   }
-  console.log(array);
+  // console.log(array);
   return newArr;
 }
 
 module.exports = chunk;
 
 
-function chunk(array, size) {
-  const chunked = [];
-  for (let element of array) {
-    // retrieve the last array element in chunked array to see if we can add
-    //more items to it
-    const last = chunked[chunked.length-1];
-    //if chunked is empty, or last is full
-    if (!last || last.length === size) {
-      // add an new array element to chunked
-      chunked.push([element]);
-    // add element to last array element in chunked
-    } else {last.push(element)}
-  }
-  return chunked;
-}
+// function chunk(array, size) {
+//   const chunked = [];
+//   for (let element of array) {
+//     // retrieve the last array element in chunked array to see if we can add
+//     //more items to it
+//     const last = chunked[chunked.length-1];
+//     //if chunked is empty, or last is full
+//     if (!last || last.length === size) {
+//       // add an new array element to chunked
+//       chunked.push([element]);
+//     // add element to last array element in chunked
+//     } else {last.push(element)}
+//   }
+//   return chunked;
+// }
